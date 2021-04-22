a4cda0ba371b696885a6af015f2b427bcde4b64f
Radovan Bast <bast@users.noreply.github.com>
2021-04-22 21:10:45 +0200

    update test; closes #5

--- test.sh
+++ test.sh
@@ -1,13 +1,24 @@
 set -e
 set -x
 
-echo $PATH
-echo ===
-env | sort
-echo ===
-
+# test python version
 python -c 'import sys; assert sys.version_info.major>=3'
-which pytest
+
+# test imports
+python -c "import click"
+python -c "import jupyter"
+python -c "import jupyterlab"
+python -c "import matplotlib"
+python -c "import numpy"
+python -c "import pandas"
 python -c "import pytest"
-python -c 'import sphinx'
+python -c "import seaborn"
+python -c "import snakemake"
+python -c "import sphinx"
 python -c "import sphinx_rtd_theme"
+
+# test a couple of command line tools
+jupyter-lab --version
+pytest --version
+sphinx-build --version
+snakemake --cores 1 --snakefile test/Snakefile
